abff8fd71be31f3db6c9319c1f896b3c5a10c1e3
Astitva7717 <astitvaaggarwal7717@gmail.com>
2022-07-17 22:30:12 +0530

    standard Input output and error
    STDIN STDOUT STDERR

--- basic3.sh
+++ basic3.sh
@@ -1,7 +1,7 @@
 #! /usr/bin/bash
 
 
-
+# standard input(STDIN)
 # one can take inputs/arguments like this
 # '$0' gives the name of file
 
@@ -17,3 +17,29 @@ echo ${args[0]} ${args[1]} ${args[2]}
 # '$#' is used to check the number of input/arguments
 
 echo $#
+
+# standard output/error
+
+: '
+all the output form a file or command is stored in 
+standard output file (STDOUT) and all the errors are stored
+in standard error file (STDERR)'
+
+# stdout
+
+ls -l  # correct cmd
+
+# stderr
+
+lsl -l # incorrect cmd
+
+# we distinguish between standard output and standard error by using redirection
+
+# here the output will store in 'file.txt' file and the error will store in 'file.err' file.
+# stdout
+
+ls -l  1>file.txt
+
+# stderr
+
+lsl -l 2>file.err
