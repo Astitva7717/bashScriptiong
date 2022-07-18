58040f0c993888cd0d187cca521416663f18b230
Astitva7717 <astitvaaggarwal7717@gmail.com>
2022-07-18 15:35:26 +0530

    create basic6.sh
    fucntions

---
+++ basic6.sh
@@ -0,0 +1,21 @@
+#! /usr/bin/bash
+
+# Fucntions
+
+# we can declare function like this
+
+function ashu(){
+    echo "hello love"
+}
+
+ashu  #calling the function
+
+# fucntion ex1
+
+function exe(){
+    read firsname
+    read secondname
+    echo hello $firsname $secondname
+}
+
+exe
