721075edf0e7fac14c9ba2739877bc86fc0ce197
Astitva7717 <astitvaaggarwal7717@gmail.com>
2022-07-18 15:22:33 +0530

    some array operations

--- basic5.sh
+++ basic5.sh
@@ -4,5 +4,29 @@
 
 # one can use () to collect the array value
 
-myarr=('she','he','her','him')
-echo ${myarr[$@]}
+myarr=('she' , 'he' , 'her' , 'him')
+echo ${myarr[@]}
+
+echo "${myarr[0]}"  #retrieving array value using index
+echo "${myarr[1]}"
+echo "${myarr[2]}"
+
+# to check the indexing of the array
+
+echo "${!myarr[@]}"
+
+# to check the number of elements in the array
+
+echo "${#myarr[@]}"
+
+# we can 'unset' the value of a particular index in an array
+
+unset myarr[2]
+unset myarr[5]
+
+echo ${myarr[@]}
+
+# in the same way on can set new value to any index in an array
+
+myarr[2]='they'
+echo ${myarr[@]}
