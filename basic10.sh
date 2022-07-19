af5878ce44f98c3d4eeaa0dfc3d0f83c7dcef8f8
Astitva7717 <astitvaaggarwal7717@gmail.com>
2022-07-19 14:49:16 +0530

    sed cmds

--- basic10.sh
+++ basic10.sh
@@ -10,8 +10,15 @@ SED command in unix supports regular expression which allows it perform complex 
  sed OPTIONS... [SCRIPT] [INPUTFILE...]
 '
 
-fname=test.log
+fname=test1.log
 
-#replace the matching occurance with another word
+# replace the matching occurance with another word
 
 sed "s/testing/resting/" $fname 
+
+# replace all matching occurance with another word
+
+sed "s/bye/hii/g" $fname
+
+# replace the changes directly into the file
+sed -i "s/hii/bye/g" $fname
