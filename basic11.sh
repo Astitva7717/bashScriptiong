25fcc95e92a2bd83ed8c65602541bce3269c2b95
Astitva7717 <astitvaaggarwal7717@gmail.com>
2022-07-19 15:10:25 +0530

    get cmd

--- basic11.sh
+++ basic11.sh
@@ -1,3 +1,18 @@
 #! /usr/bin/bash
 
 # curl
+
+# GET call
+
+#curl -s http://dummy.restapiexample.com/api/v1/employees
+curl -s https://www.linkedin.com/in/astitvaaggarwal/
+if [ $? -eq 0 ]
+then
+    echo "\n"
+    echo "============"
+    echo "get call successfull"
+else
+    echo "\n"
+    echo "============="
+    echo "get call failed"
+fi
